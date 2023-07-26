48b816c1c2024e5e1f41a673886de4e4ddc69858
Andrew Levin <alevin@gpu012.nsls2.bnl.local>
2023-07-26 00:30:01 -0400

    lots of notebook progress, still needs refining & work

--- nsls2_notebooks/cms-giwaxs_processing.py
+++ nsls2_notebooks/cms-giwaxs_processing.py
@@ -3,7 +3,6 @@ This script takes the CMS sample paths generated for series measurements, separa
 sets of scan file paths, and outputs an xr.DataSet for the ex situ measurements or outputs a xr.DataArray(s) for in situ time-resolved measurements.
 """
 
-### Imports:
 
 ### Imports:
 import pathlib
@@ -14,8 +13,3 @@ import xarray as xr
 import PyHyperScattering as phs
 import pygix
 from tqdm.auto import tqdm  # progress bar loader!
-
-# Load custom pygix functions
-pg_convert = phs.util.GIWAXS.pg_convert  
-pg_convert_series = phs.util.GIWAXS.pg_convert_series
-
